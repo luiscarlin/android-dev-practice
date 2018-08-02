eeafd302841c9f05c6a2ad418974cbd83f999f7c
Luis Carlin <luiscarlin@gmail.com>
2018-08-02 07:04:53 -0400

    Add test

---
+++ quarantine/app/src/test/java/com/luchoc/quarantine/MainActivityImplTest.java
@@ -0,0 +1,21 @@
+package com.luchoc.quarantine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import static org.mockito.Mockito.*;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MainActivityImplTest {
+
+    @Mock
+    MainActivityQtn qtn;
+
+    @Test
+    public void displays_hello_world_on_create() {
+        MainActivityImpl impl = new MainActivityImpl(qtn);
+        impl.onCreate();
+        verify(qtn, times(1)).setContentView(R.layout.activity_main);
+    }
+}
