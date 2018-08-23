b5ee804fe1427504ccd7fb5677d259d907a5de72
Luis Carlin <luiscarlin@gmail.com>
2018-08-23 06:04:47 -0400

    Start retrofit oauth

---
+++ retrofit-oauth/app/src/main/java/com/luchoc/retrofitoauth/MainActivity.java
@@ -0,0 +1,15 @@
+package com.luchoc.retrofitoauth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String clientId =
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
