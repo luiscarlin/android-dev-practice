b35d51c84d3becde43466494cf7210ef2abbdf8d
Luis Carlin <luiscarlin@gmail.com>
2018-09-18 05:35:12 -0400

    Old stuff

--- retrofit-oauth/app/src/main/java/com/luchoc/retrofitoauth/MainActivity.java
+++ retrofit-oauth/app/src/main/java/com/luchoc/retrofitoauth/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import retrofit2.Call;
@@ -30,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
+        Log.d("URL FROM GITHUB", intent.getData().toString());
 
         Uri uri = intent.getData();
 
