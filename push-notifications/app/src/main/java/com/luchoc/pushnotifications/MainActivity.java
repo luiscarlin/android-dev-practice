f325b70dd59976f887864af25f5a8665b7dc22c1
Luis Carlin <luiscarlin@gmail.com>
2018-09-19 07:38:20 -0400

    Display a notification after creating a channel

--- push-notifications/app/src/main/java/com/luchoc/pushnotifications/MainActivity.java
+++ push-notifications/app/src/main/java/com/luchoc/pushnotifications/MainActivity.java
@@ -1,7 +1,13 @@
 package com.luchoc.pushnotifications;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createNotificationChannel();
     }
+
+    public void sendNotification(View view) {
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext(), "MYCHANNEL")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("TItle")
+                .setContentText("ContentText")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(123, mBuilder.build());
+    }
+
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "MYCHANNEL";
+            String description = "CHANNELFORTESTING";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("MYCHANNEL", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
 }
